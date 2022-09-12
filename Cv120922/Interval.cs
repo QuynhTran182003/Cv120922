cd0476cba868b857046cc4af343d9d9aae49a5e5
tran2 <tran2@PC980>
2022-09-12 09:03:41 +0200

    Add project files.

---
+++ Cv120922/Interval.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cv120922
+{
+    internal class Interval
+    {
+        private int low;
+        private int high;
+
+        public Interval(int low, int high)
+        {
+            this.low = low;
+            this.high = high;
+        }
+        public Interval() { }
+        // find intersection between 2 intervals
+        public static Interval? intersection(Interval i1, Interval i2)
+        {
+            if (i1.low > i2.low)
+            {
+                //swaping 
+                Interval temp = new Interval();
+                temp = i1;
+                i1 = i2;
+                i2 = temp;
+            }
+
+            Interval? prunik = null;
+            if (i2.low > i1.low && i2.high < i1.high)
+            {
+                prunik = i2;
+            } 
+            else if(i1.high > i2.low)
+            {
+                prunik = new Interval(i2.low, i1.high);
+                
+            }
+            return prunik;
+        }
+        
+        public int Low { 
+            get { return low; }
+            set 
+            {
+                if (value < low)
+                {
+                    throw new Exception("Low value must not be larger or equal to high value.");
+                }
+            }
+        }
+        public int High { get; set; }
+
+        //display
+        public override string ToString()
+        {
+            return "Interval <" + this.low + ", " + this.high + ">"; 
+        }
+    }
+}
---
+++ Cv120922/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Cv120922
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Interval i = new Interval(10, 60);
+            Interval i2 = new Interval(15, 20); 
+
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(i2.ToString());
+            Console.WriteLine("Intersection: " + Interval.intersection(i, i2));
+        }
+    }
+}
